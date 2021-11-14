e10e954700b5a10e8def6a180e00dce39e13c6de
李梦彤 <90666887+bbzzpws@users.noreply.github.com>
2021-11-14 20:42:06 +0800

    添加了tabber、宫格、头像

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,115 +1,38 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-   <i-button bind:click="handleClick"
-              type="primary">Primary</i-button>
-    <i-button bind:click="handleClick"
-              type="ghost">Ghost</i-button>
-    <i-button bind:click="handleClick"
-              type="info">Info</i-button>
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <van-tag>标签</van-tag>
-    <van-tag type="danger">标签</van-tag>
-    <van-tag type="primary">标签</van-tag>
-    <van-tag type="success">标签</van-tag>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter" class="counter">去往Vuex示例页面</a>
-  </div>
+<div>
+<home></home>
+<i-tab-bar class="flex" :current="current" color="#f759ab" @change="handleChange">
+    <i-tab-bar-item key="homepage" icon="homepage" current-icon="homepage_fill" title="首页"></i-tab-bar-item>
+    <i-tab-bar-item key="group" icon="group" current-icon="group_fill" title="朋友"></i-tab-bar-item>
+    <i-tab-bar-item key="remind" icon="remind" current-icon="remind_fill" title="通知"></i-tab-bar-item>
+    <i-tab-bar-item key="mine" icon="mine" current-icon="mine_fill" title="我的"></i-tab-bar-item>
+</i-tab-bar>
+</div>
 </template>
-
 <script>
-import card from '@/components/card'
+import home from '@/components/home'
 
 export default {
-  mpType: 'page',
-
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {}
+      current: 'homepage'
     }
   },
-
   components: {
-    card
+    home
   },
-
   methods: {
-    bindViewTap () {
-      const url = '/packageA/logs'
-      this.$router.push(url)
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      // eslint-disable-next-line
-      console.log('clickHandle:', msg, ev)
+    handleChange (detail) {
+      this.current = detail.mp.detail.key
     }
-  },
-
-  created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
   }
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+<style>
+.flex{
+  position:absolute;
+  width:100%;
+  top:90%;
 }
 </style>
