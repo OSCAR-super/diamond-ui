4217b36d3173146cd1289c2c2eca4d5da749c1ce
liyutian <liyutian>
2021-11-15 11:10:17 +0800

    设置搜索框

--- src/components/indexpage.vue
+++ src/components/indexpage.vue
@@ -1,11 +1,15 @@
 <template>
 <div>
-<i-panel title="圆角输入框">
-    <i-input :value="value1" type="number" mode="wrapped" placeholder="请输入搜索词" />
-</i-panel>
-<div class = "searching">
-  <i-button bind class = "searching":click="handleClick" type="primary" shape="circle" size="small">搜索</i-button>
-</div>
+<i-row>
+    <i-col span="18" i-class="col-class">
+        <i-panel title="圆角输入框">
+          <i-input :value="value1" type="number" mode="wrapped" placeholder="请输入搜索词" />
+        </i-panel>
+    </i-col>
+    <i-col span="5" i-class="col-class">
+      <i-button bind inline="true" class = "searching":click="handleClick" type="primary" shape="square" size="small">搜索</i-button>
+    </i-col>
+</i-row>
 <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
     <view slot="content">内容不错</view>
     <view slot="footer">尾部内容</view>
@@ -25,9 +29,4 @@ export default {
   }
 }
 </script>
-<style>
-.searching{
-  width:150px;
-  text-align: center;
-}
 </style>
