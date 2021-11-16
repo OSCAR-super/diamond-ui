cbb4a59b71af273d330d2af2448a783cd162f75f
liyutian <liyutian>
2021-11-16 17:06:26 +0800

    修改评分错误

--- src/components/smile.vue
+++ src/components/smile.vue
@@ -13,44 +13,22 @@
 <i-cell title="禁用点击和手势选择星">
         <i-rate 
             disabled="true"
-            value="starIndex4">
-            starIndex4星
+            :value="starIndex">
+            4星
         </i-rate>
     </i-cell>
 </i-cell-group>
 </div>
 </template>
+<script>
 export default {
   data () {
     return {
-      starIndex4 : 4
+      starIndex: 4
     }
-  },
-  onChange1(e){
-        const index = e.detail.index;
-        this.setData({
-            'starIndex1' : index
-        })
-    },
-    onChange2(e){
-        const index = e.detail.index;
-        this.setData({
-            'starIndex2' : index
-        })
-    },
-    onChange3(e){
-        const index = e.detail.index;
-        this.setData({
-            'starIndex3' : index
-        })
-    },
-    onChange5(e){
-        const index = e.detail.index;
-        this.setData({
-            'onChange5' : index
-        })
-    }
-});
+  }
+}
+</script>
 <style>
 .avatar1{
   text-align: center;
